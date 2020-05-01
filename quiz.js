5c5a538145183dad0c4c1706acaa821e1ac0246c
Ike Ekwueme <cryptoekwu@gmail.com>
2020-05-01 18:24:08 -0500

    created HTML and questions

---
+++ quiz.js
@@ -0,0 +1,27 @@
+let questions = [
+    {
+        title: "What is the name of Homer Simpson's son?",
+        choices: ["Bart", "Chowder", "Miguel", "Homer Jr."],
+        answer: "Bart"
+    },
+    {
+        title: "How many licks does it take to get to the center of a tootsie pop?",
+        choices: ["1", "2", "3", "The world may never know."],
+        answer: "The world may never know."
+    },
+    {
+        title: "Who is the 7th Hokage?",
+        choices: ["Shikamaru", "Kakashi", "Naruto", "Donald Trump"],
+        answer: "Naruto"
+    },
+    {
+        title: "How old is Aang when avatar begins?",
+        choices: ["12", "112","56","72"],
+        answer: "112"
+    },
+    {
+        title: "How many seconds do you lose when you get an answer wrong on this quiz?",
+        choices: ["15", "29", "20", "34"],
+        answer: "15"
+    }
+]
